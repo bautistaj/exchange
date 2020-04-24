0e882ec174133b5341b8d79bc21b321f8f0d0bb9
Jose Luis Campos Bautista <bautistaj20@hotmail.com>
2020-04-24 17:35:53 -0500

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <main id="app">
+    <btaj-header />
+    <router-view class="container px-5 sm:px-20 py-20 flex justify-center" />
+  </main>
 </template>
-
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import BtajHeader from '@/components/BtajHeader';
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    BtajHeader
   }
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <btaj-assets-table />
+  </div>
+</template>
+
+<script>
+import BtajAssetsTable from '@/components/BtajAssetsTable';
+
+export default {
+  name: "Home",
+  components: { BtajAssetsTable }
+};
+</script>
